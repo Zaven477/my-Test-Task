c53e68a268feb2e15469a10ba4abd3ea60ff910a
Zaven477 <Papikyan86@bk.ru>
2025-10-05 13:49:12 +0300

    connecting redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRouter } from "./AppRouter/AppRouter";
 
-const queryClient = new QueryClient();
-
 export const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AppRouter />
-    </QueryClientProvider>
-  );
+  return <AppRouter />;
 };
