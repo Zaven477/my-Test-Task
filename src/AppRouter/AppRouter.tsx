99b9535a0a4ba60740b0372bb5cea7aaa150cea4
Zaven477 <Papikyan86@bk.ru>
2025-10-05 13:51:01 +0300

    adding a new route

--- src/AppRouter/AppRouter.tsx
+++ src/AppRouter/AppRouter.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import { UserDataForm } from "../components/UserDataForm/UserDataForm";
 import { EmploymentAddressForm } from "../components/EmploymentAddressForm/EmploymentAddressForm";
 import { ROUTES } from "./routes";
+import { UserLoanParametersForm } from "../components/UserLoanParametersForm/UserLoanParametersForm";
 
 export const AppRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRouter = () => {
       <Routes>
         <Route path={ROUTES.USER_DATA} element={<UserDataForm />} />
         <Route path={ROUTES.WORK_ADDRESS} element={<EmploymentAddressForm />} />
+        <Route path={ROUTES.LOAN} element={<UserLoanParametersForm />} />
       </Routes>
     </BrowserRouter>
   );
--- src/AppRouter/routes.ts
+++ src/AppRouter/routes.ts
@@ -1,4 +1,5 @@
 export const ROUTES = {
   USER_DATA: "/",
   WORK_ADDRESS: "/work-address",
+  LOAN: '/loan',
 } as const;
